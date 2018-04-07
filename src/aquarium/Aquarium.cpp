2ecaf4b762cab208572ff9b41ce11d40a0aa4afd
yusufrahmatp <yusufrahmatp@gmail.com>
2018-04-07 18:26:23 +0700

    Change add and remove method in Aquarium based on the value of LinkedList

--- src/aquarium/Aquarium.cpp
+++ src/aquarium/Aquarium.cpp
@@ -77,19 +77,23 @@ void Aquarium::updateState(double current_time) {
 }
 
 void Aquarium::createPiranha() {
-	content_piranha.add(new Piranha( /* TBD */ ));
+	Piranha* new_elmt = new Piranha (/* TBD */);
+	content_piranha.add(new_elmt);
 }
 
 void Aquarium::createGuppy() {
-	content_guppy.add(new Guppy(curr_time, this));
+	Guppy* new_elmt = new Guppy(curr_time, this)
+	content_guppy.add(new_elmt);
 }
 
 void Aquarium::createSnail() {
-	content_snail.add(new Snail( /* TBD */ ));
+	Snail* new_elmt = new Snail( /* TBD */ );
+	content_snail.add(new_elmt);
 }
 
 void Aquarium::createPellet() {
-	content_pellet.add(new Pellet( /* TBD */ ));
+	Pellet* new_elmt = new Pellet( /* TBD */ );
+	content_pellet.add(new_elmt);
 }
 
 void Aquarium::createCoin(double x, double y, int value) {
@@ -97,21 +101,21 @@ void Aquarium::createCoin(double x, double y, int value) {
 }
 
 void Aquarium::deletePiranha(Piranha* p) {
-	content_piranha.remove(*p);
+	content_piranha.remove(p);
 }
 
 void Aquarium::deleteGuppy(Guppy* g) {
-	content_guppy.remove(*g);
+	content_guppy.remove(g);
 }
 
 void Aquarium::deleteSnail(Snail* s) {
-	content_snail.remove(*s);
+	content_snail.remove(s);
 }
 
 void Aquarium::deletePellet(Pellet* p) {
-	content_pellet.remove(*p);
+	content_pellet.remove(p);
 }
 
 void Aquarium::deleteCoin(Coin* c) {
-	content_coin.remove(*c);
+	content_coin.remove(c);
 }
