1f095116b38b479b09b3c7f336641010f7d2caf5
Abram Perdanaputra Situmorang <abram.perdanaputra@gojek.com>
2021-03-14 11:35:53 +0700

    Migrate pellet test to gtest

--- test/testpellet.cpp
+++ test/testpellet.cpp
@@ -1,89 +1,37 @@
 /* Driver file for Pellet */
-#include <iostream>
+#include <gtest/gtest.h>
 #include "Aquarium.hpp"
 #include "Pellet.hpp"
-using namespace std;
 
-int main() {
-	int ret;
-	double x, y;
-	Aquarium a(0, 0, 480, 640);
-	Pellet c(5, 5, &a);
+class PelletTest: public ::testing::Test {
+protected:
+  void SetUp() override {
+    aq = new Aquarium(0, 0, 480, 640);
+    aq->createPellet(5, 5);
+    p = aq->getPelletList().get(0);
+    x = p->getX();
+    y = p->getY();
+  }
 
-	cout << "#######################" << endl;
-	cout << "# RUNNING PELLET TEST #" << endl;
-	cout << "#######################" << endl;
+  void TearDown() override {
+    delete aq;
+  }
 
-	/* Test 1 */
-	cout << "Running TEST 1" << endl;
-	x = c.getX();
-	y = c.getY();
-	a.setCurrTime(100);
-	c.updateState();
-	ret = y <= c.getY() ? 1 : 0;
-	cout << "	TEST 1: ";
-	if(ret == 1) {
-		cout << "SUCCESS" << endl;
-	} else {
-		cout << "FAIL" << endl;
-	}
+  Aquarium *aq;
+  Pellet *p;
+  double x, y;
+};
 
-	/* Test 2 */
-	cout << "Running TEST 2" << endl;
-	x = c.getX();
-	y = c.getY();
-	a.setCurrTime(200);
-	c.updateState();
-	ret = y <= c.getY() ? 1 : 0;
-	cout << "	TEST 2: ";
-	if(ret == 1) {
-		cout << "SUCCESS" << endl;
-	} else {
-		cout << "FAIL" << endl;
-	}
+TEST_F(PelletTest, ForwardTime100ms_Should_GoDownYAxis_And_StayOnXAxis) {
+  aq->setCurrTime(100);
+  p->updateState();
+  EXPECT_LT(y, p->getY());
+  EXPECT_DOUBLE_EQ(x, p->getX());
+}
 
-	/* Test 3 */
-	cout << "Running TEST 3" << endl;
-	x = c.getX();
-	y = c.getY();
-	a.setCurrTime(300);
-	c.updateState();
-	ret = y <= c.getY() ? 1 : 0;
-	cout << "	TEST 3: ";
-	if(ret == 1) {
-		cout << "SUCCESS" << endl;
-	} else {
-		cout << "FAIL" << endl;
-	}
-
-	/* Test 4 */
-	cout << "Running TEST 4" << endl;
-	x = c.getX();
-	y = c.getY();
-	a.setCurrTime(400);
-	c.updateState();
-	ret = x == c.getX() ? 1 : 0;
-	cout << "	TEST 4: ";
-	if(ret == 1) {
-		cout << "SUCCESS" << endl;
-	} else {
-		cout << "FAIL" << endl;
-	}
-
-	/* Test 5 */
-	cout << "Running TEST 5" << endl;
-	x = c.getX();
-	y = c.getY();
-	a.setCurrTime(500);
-	c.updateState();
-	ret = x == c.getX() ? 1 : 0;
-	cout << "	TEST 5: ";
-	if(ret == 1) {
-		cout << "SUCCESS" << endl;
-	} else {
-		cout << "FAIL" << endl;
-	}
-
-	cout << endl << endl;
-
+TEST_F(PelletTest, ForwardTime200ms_Should_GoDownYAxis_And_StayOnXAxis) {
+  aq->setCurrTime(200);
+  p->updateState();
+  EXPECT_LT(y, p->getY());
+  EXPECT_DOUBLE_EQ(x, p->getX());
 }
