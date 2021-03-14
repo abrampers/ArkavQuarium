8f001345ed43fbfa1a19fb6bce09866c20159b69
Abram Perdanaputra Situmorang <abram.perdanaputra@gojek.com>
2021-03-14 11:46:13 +0700

    Migrate snail test to gtest

--- test/testsnail.cpp
+++ test/testsnail.cpp
@@ -1,44 +1,40 @@
 /* Driver file for Snail */
-#include <iostream>
+#include <gtest/gtest.h>
 #include "Aquarium.hpp"
 #include "Snail.hpp"
-using namespace std;
 
-int main() {
-	int ret;
-	double y;
-	Aquarium a(0, 0, 480, 640);
-	Snail *s = a.getSnailList().get(0);
+class SnailTest: public ::testing::Test {
+protected:
+  void SetUp() override {
+    aq = new Aquarium(0, 0, 480, 640);
+    s = aq->getSnailList().get(0);
+  }
 
-	cout << "######################" << endl;
-	cout << "# RUNNING SNAIL TEST #" << endl;
-	cout << "######################" << endl;
+  Aquarium *aq;
+  Snail *s;
+};
 
-	/* Test 1 */
-	cout << "Running TEST 1" << endl;
-	a.createCoin(s->getX(), s->getY(), 100);
-	s->updateState();
-	ret = a.getCoinList().getLength() == 0 ? 1 : 0;
-	cout << "	TEST 1: ";
-	if(ret == 1) {
-		cout << "SUCCESS" << endl;
-	} else {
-		cout << "FAIL" << endl;
-	}
+TEST_F(SnailTest, GetCoinOnTheSameCoordinate) {
+  aq->createCoin(s->getX(), s->getY(), 100);
+  s->updateState();
+  EXPECT_EQ(0, aq->getCoinList().getLength());
+}
 
-	/* Test 2 */
-	cout << "Running TEST 2" << endl;
-	a.setCurrTime(200);
-	y = s->getY();
-	s->updateState();
-	ret = y == s->getY() ? 1 : 0;
-	cout << "	TEST 2: ";
-	if(ret == 1) {
-		cout << "SUCCESS" << endl;
-	} else {
-		cout << "FAIL" << endl;
-	}
+TEST_F(SnailTest, NoCoin_SnailShouldNotMove) {
+  aq->setCurrTime(200);
+  double x = s->getX();
+  double y = s->getY();
+  s->updateState();
+  EXPECT_DOUBLE_EQ(x, s->getX());
+  EXPECT_DOUBLE_EQ(y, s->getY());
+}
 
-	cout << endl << endl;
-
+TEST_F(SnailTest, CoinPresent_SnailShouldMoveToCoin) {
+  aq->createCoin(s->getX() + 100, s->getY() + 100, 10);
+  double x = s->getX();
+  double y = s->getY();
+  aq->setCurrTime(10);
+  s->updateState();
+  EXPECT_LT(x, s->getX());
+  EXPECT_DOUBLE_EQ(y, s->getY());
 }
