2a7f5058012d78fb29e5d98e68f5c260c86bc066
Abram Situmorang <abram.perdanaputra@gmail.com>
2021-03-14 20:53:14 +0700

    Fix casting

--- src/Helper.cpp
+++ src/Helper.cpp
@@ -1,6 +1,6 @@
 #include "Helper.hpp"
 
 double fRand(double fMin, double fMax) {
-    double f = (double)rand() / RAND_MAX;
+    double f = static_cast<double>(rand()) / RAND_MAX;
     return fMin + f * (fMax - fMin);
 }
